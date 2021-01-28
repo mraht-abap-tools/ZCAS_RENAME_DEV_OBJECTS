f85d9491dcdec11adb752c9aff33cff620a75167
mariusraht2 <mariusraht+2@gmail.com>
2021-01-28 16:58:26 +0100

    Remove obsolete files

--- calculator.py
+++
@@ -1,4 +0,0 @@
-x = input("X: ")
-y = input("Y: ")
-z = int(x) * int(y)
-print(z)
--- split.py
+++
@@ -1,5 +0,0 @@
-import re
-
-file = str('D:\Downloads\Test-master\src\1\ziot_r_test.prog.xml')
-filename = re.split(r'((?i)ZIOT_\w+)\.\w+', file)
-print(filename)

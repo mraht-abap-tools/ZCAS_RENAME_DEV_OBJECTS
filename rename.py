4c374c30fb0e89dcc66e4fbea839c7f5f731f333
mariusraht1 <mariusraht+1@gmail.com>
2022-08-04 22:45:35 +0200

    Optimizations and fixes
    * Replace all occurrences of old file name in file name
    * Searching for occurrences in files is faster

--- rename.py
+++ rename.py
@@ -52,36 +52,41 @@ for r, d, f in os.walk(newPathToGitFolder):
     for file in f:
         if not re.search(rf'(?i)\.bak', file) and re.search(rf'(?i)({oldNamespace}|{newNamespace})', file):
             filePath = r
-            fileSegments = re.search(r'^([\w#\s]+)(\..+)$',file)
+            fileSegments = re.search(r'^(([\w#]+)[\.\w#\s]+)(\..+)$',file)
             if fileSegments is not None:
                 fileName = fileSegments.group(1)
-                fileExtension = fileSegments.group(2)
+                objectName = fileSegments.group(2)
+                fileExtension = fileSegments.group(3)
 
             newFilename = re.sub(rf'(?i){oldNamespace}', newNamespace, fileName)
             if excludedObjectsFile is None or not (re.search(rf'(?i){fileName}', excludedObjects)):
-                filesToRename.append([filePath, fileName, fileExtension])
+                filesToRename.append([filePath, fileName, fileExtension, objectName])
             else:
                 info(rf'Exclude {fileName} and {newFilename} from renaming')
                 newFilename = fileName
 
-            files.append([filePath, newFilename, fileExtension])
+            files.append([filePath, newFilename, fileExtension, objectName])
 
 info('3) Renaming files...')
 for file in filesToRename:
     filePath = file[0]
     fileName = file[1]
     fileExtension = file[2]
+    objectName = file[3]
+
     newFilename = ''
+    newObjectName = ''
 
     if re.search(rf'(?i){oldNamespace}', fileName):
         newFilename = re.sub(rf'(?i){oldNamespace}', newNamespace, fileName)
-
+        newObjectName = re.sub(rf'(?i){oldNamespace}', newNamespace, objectName)
         oldFilepath = os.path.join(filePath, fileName + fileExtension)
         newFilepath = os.path.join(filePath, newFilename + fileExtension)
 
         try:
             shutil.move(oldFilepath, newFilepath)
             file[1] = newFilename
+            file[3] = newObjectName
             info(rf'{fileName}{fileExtension} => {newFilename}{fileExtension}')
         except BaseException:
             error(rf'Error: Renaming {fileName} to {newFilename} failed.')
@@ -90,17 +95,24 @@ for file in filesToRename:
         newFilename = fileName
 
 info('4) Renaming occurrences within files...')
+
+oldObjectName = ''
 totalNumberOfExec = len(filesToRename) * len(files)
+sorted(filesToRename, key=lambda x: x[3])
+
 for index1, file in enumerate(filesToRename):
     # Each development object has a xml file. Some files just contain e.g. source code instead of
     # metadata of a development object. Thus no search for occurrences of these development objects
     # are necessary because the search is being done with the related xml file.
     if (re.search(rf'(?i).xml$', file[2])):
-        oldObjectName = re.sub(rf'(?i){newNamespace}', oldNamespace, file[1]).replace('#', '/').upper()
-        newObjectName = re.sub(rf'(?i){oldNamespace}', newNamespace, file[1]).replace('#', '/').upper()
+        tmpOldObjectName = re.sub(rf'(?i){newNamespace}', oldNamespace, file[3]).replace('#', '/').upper()
+        if oldObjectName != '' and oldObjectName == tmpOldObjectName:
+            continue
+        oldObjectName = tmpOldObjectName
+        newObjectName = re.sub(rf'(?i){oldNamespace}', newNamespace, file[3]).replace('#', '/').upper()
         for index2, file2 in enumerate(files):
             filePath = os.path.join(file2[0], file2[1] + file2[2])
-            print('%-50s' % oldObjectName + rf': {round(((((index1 - 1) * len(files)) + (index2 + 1)) / totalNumberOfExec) * 100, 2)}%',"\r", end=' ')
+            print('%-50s' % oldObjectName + rf': {round(((((index1) * len(files)) + (index2 + 1)) / totalNumberOfExec) * 100, 2)}%',"\r", end=' ')
             if os.path.exists(filePath):
                 with open (filePath, 'r+', encoding="utf8") as f:
                     try:

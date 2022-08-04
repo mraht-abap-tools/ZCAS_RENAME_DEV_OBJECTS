890577f538241d3306bc6a1110aa6b305abab3e1
Marius Raht <99023738+mraht@users.noreply.github.com>
2022-08-04 21:39:59 +0200

    Update rename.py
    Changes:
    * Files are now copied first to "src_renamed" and renamed there only
    * Files are being replaced by the new file name
    * Added better logging
    
    Fixes:
    * If excludedObjectsFile is not found, application doesn't dump anymore
    * If fileSegments is not found, application doesn't dump anymore

--- rename.py
+++ rename.py
@@ -1,7 +1,8 @@
-# Version: 29.01.2021-001
+# Version: 04.08.2022-001
 
 import csv
 import logging
+from msilib.schema import Error
 import os
 import re
 import shutil
@@ -15,10 +16,10 @@ def error(msg):
     logging.error(msg)
     print(msg)
 
-pathToGitFolder = input("Path to abapGit repo: ") + "\\src"
-while not os.path.isdir(pathToGitFolder):
+pathToGitFolder = input("Path to abapGit repo: ")
+while not os.path.isdir(pathToGitFolder + "\\src"):
     print("Entered folder doesn't exist or isn't an abapGit repository.")
-    pathToGitFolder = input("Path to abapGit repo: ") + "\\src"
+    pathToGitFolder = input("Path to abapGit repo: ")
 oldNamespace = input("Old namespace: ")
 newNamespace = input("New namespace: ")
 
@@ -27,29 +28,37 @@ logging.basicConfig(level=logging.DEBUG, filename="log.txt", filemode="a+",
 
 info('****************************************************************************************************')
 
-# Consider excluded files/development objects
-excludedObjectsFile = open('exclude.csv', 'r')
-excludedObjects = excludedObjectsFile.read()
-excludedObjectsFile.close()
-excludedObjects = excludedObjects.replace('/', '#').upper()
-
+info('1) Copy files and prepare renaming...')
 oldNamespace = oldNamespace.replace('/', '#').lower()
 newNamespace = newNamespace.replace('/', '#').lower()
 
-info('1) Determine relevant files...')
+# Consider excluded files/development objects
+excludedObjectsFile = None
+if os.path.exists('exclude.csv'):
+    excludedObjectsFile = open('exclude.csv', 'r')
+    excludedObjects = excludedObjectsFile.read()
+    excludedObjectsFile.close()
+    excludedObjects = excludedObjects.replace('/', '#').upper()
+
+newPathToGitFolder = pathToGitFolder + '\src_renamed'
+pathToGitFolder += '\src'
+shutil.copytree(pathToGitFolder, newPathToGitFolder, dirs_exist_ok=True)
+
+info('2) Determine relevant files...')
 files = []
 filesToRename = []
 
-for r, d, f in os.walk(pathToGitFolder):
+for r, d, f in os.walk(newPathToGitFolder):
     for file in f:
         if not re.search(rf'(?i)\.bak', file) and re.search(rf'(?i)({oldNamespace}|{newNamespace})', file):
             filePath = r
-            fileSegments = re.search(r'^([\w#]+)(\..+)$',file)
-            fileName = fileSegments.group(1)
-            fileExtension = fileSegments.group(2)
+            fileSegments = re.search(r'^([\w#\s]+)(\..+)$',file)
+            if fileSegments is not None:
+                fileName = fileSegments.group(1)
+                fileExtension = fileSegments.group(2)
 
             newFilename = re.sub(rf'(?i){oldNamespace}', newNamespace, fileName)
-            if not (re.search(rf'(?i){fileName}', excludedObjects)):
+            if excludedObjectsFile is None or not (re.search(rf'(?i){fileName}', excludedObjects)):
                 filesToRename.append([filePath, fileName, fileExtension])
             else:
                 info(rf'Exclude {fileName} and {newFilename} from renaming')
@@ -57,7 +66,7 @@ for r, d, f in os.walk(pathToGitFolder):
 
             files.append([filePath, newFilename, fileExtension])
 
-info('2) Renaming files...')
+info('3) Renaming files...')
 for file in filesToRename:
     filePath = file[0]
     fileName = file[1]
@@ -69,30 +78,37 @@ for file in filesToRename:
 
         oldFilepath = os.path.join(filePath, fileName + fileExtension)
         newFilepath = os.path.join(filePath, newFilename + fileExtension)
-        if shutil.move(oldFilepath, newFilepath):
+
+        try:
+            shutil.move(oldFilepath, newFilepath)
+            os.remove(oldFilepath)
             file[1] = newFilename
-            info(rf'{fileName} => {newFilename}')
-        else:
+            info(rf'{fileName}{fileExtension} => {newFilename}{fileExtension}')
+        except BaseException:
             error(rf'Error: Renaming {fileName} to {newFilename} failed.')
         
     elif re.search(rf'(?i){newNamespace}', fileName):
         newFilename = fileName
 
-info('3) Renaming occurrences within files...')
-for file in filesToRename:
+info('4) Renaming occurrences within files...')
+totalNumberOfExec = len(filesToRename) * len(files)
+for index1, file in enumerate(filesToRename):
     # Each development object has a xml file. Some files just contain e.g. source code instead of
     # metadata of a development object. Thus no search for occurrences of these development objects
     # are necessary because the search is being done with the related xml file.
     if (re.search(rf'(?i).xml$', file[2])):
         oldObjectName = re.sub(rf'(?i){newNamespace}', oldNamespace, file[1]).replace('#', '/').upper()
         newObjectName = re.sub(rf'(?i){oldNamespace}', newNamespace, file[1]).replace('#', '/').upper()
-        info(rf'Search for {oldObjectName}...')
-        for file2 in files:
+        for index2, file2 in enumerate(files):
             filePath = os.path.join(file2[0], file2[1] + file2[2])
-            info(rf'> {filePath}')
+            print('%-50s' % oldObjectName + rf': {round(((((index1 - 1) * len(files)) + (index2 + 1)) / totalNumberOfExec) * 100, 2)}%',"\r", end=' ')
             if os.path.exists(filePath):
-                with open (filePath, 'r+' ) as f:
-                    content = f.read()
+                with open (filePath, 'r+', encoding="utf8") as f:
+                    try:
+                        content = f.read()
+                    except BaseException:
+                        continue
+
                     content_new = re.sub(rf'(?i){oldObjectName}', newObjectName, content, flags = re.MULTILINE)
                     if content != content_new:
                         info(rf'>> Occurrences of {oldObjectName} replaced by {newObjectName} in {filePath}')
@@ -101,5 +117,4 @@ for file in filesToRename:
                         f.truncate()
 
 info('Executed successfully.')
-
 input('Press any key to exit...')

13a05e9dd8011a8fcf8e7d6e48f3f07445246ecf
mariusraht2 <mariusraht+2@gmail.com>
2022-10-18 17:35:42 +0200

    Replace occ. in every file

--- rename.py
+++ rename.py
@@ -49,24 +49,23 @@ info('2) Determine relevant files...')
 files = []
 filesToRename = []
 
-for r, d, f in os.walk(newPathToGitFolder):
-    for file in f:
+for filePath, dirnames, filenames in os.walk(newPathToGitFolder):
+    for file in filenames:
+        fileSegments = re.search(r'^(([\w#]+)[\.\w#\s]+)(\..+)$',file)
+        if fileSegments is not None:
+            fileName = fileSegments.group(1)
+            objectName = fileSegments.group(2)
+            fileExtension = fileSegments.group(3)        
         if not re.search(rf'(?i)\.bak', file) and re.search(rf'(?i)({oldNamespace}|{newNamespace})', file):
-            filePath = r
-            fileSegments = re.search(r'^(([\w#]+)[\.\w#\s]+)(\..+)$',file)
-            if fileSegments is not None:
-                fileName = fileSegments.group(1)
-                objectName = fileSegments.group(2)
-                fileExtension = fileSegments.group(3)
-
             newFilename = re.sub(rf'(?i){oldNamespace}', newNamespace, fileName)
             if excludedObjectsFile is None or not (re.search(rf'(?i){fileName}', excludedObjects)):
                 filesToRename.append([filePath, fileName, fileExtension, objectName])
+                files.append([filePath, newFilename, fileExtension, objectName])
             else:
                 info(rf'Exclude {fileName} and {newFilename} from renaming')
                 newFilename = fileName
-
-            files.append([filePath, newFilename, fileExtension, objectName])
+        else:
+            files.append([filePath, fileName, fileExtension, objectName])
 
 info('3) Renaming files...')
 for file in filesToRename:
@@ -97,36 +96,23 @@ for file in filesToRename:
 
 info('4) Renaming occurrences within files...')
 
-oldObjectName = ''
-totalNumberOfExec = len(filesToRename) * len(files)
-sorted(filesToRename, key=lambda x: x[3])
+for index, file in enumerate(files):
+    print('%-50s' % file[1] + rf': {round((index / len(files)) * 100, 2)}%',"\r", end=' ')
+    filePath = os.path.join(file[0], file[1] + file[2])
+    
+    if os.path.exists(filePath):
+        with open (filePath, 'r+', encoding="utf8") as f:
+            try:
+                content = f.read()
+            except BaseException:
+                continue
 
-for index1, file in enumerate(filesToRename):
-    # Each development object has a xml file. Some files just contain e.g. source code instead of
-    # metadata of a development object. Thus no search for occurrences of these development objects
-    # are necessary because the search is being done with the related xml file.
-    if (re.search(rf'(?i).xml$', file[2])):
-        tmpOldObjectName = re.sub(rf'(?i){newNamespace}', oldNamespace, file[3]).replace('#', '/').upper()
-        if oldObjectName != '' and oldObjectName == tmpOldObjectName:
-            continue
-        oldObjectName = tmpOldObjectName
-        newObjectName = re.sub(rf'(?i){oldNamespace}', newNamespace, file[3]).replace('#', '/').upper()
-        for index2, file2 in enumerate(files):
-            filePath = os.path.join(file2[0], file2[1] + file2[2])
-            print('%-50s' % oldObjectName + rf': {round(((((index1) * len(files)) + (index2 + 1)) / totalNumberOfExec) * 100, 2)}%',"\r", end=' ')
-            if os.path.exists(filePath):
-                with open (filePath, 'r+', encoding="utf8") as f:
-                    try:
-                        content = f.read()
-                    except BaseException:
-                        continue
-
-                    content_new = re.sub(rf'(?i){oldNamespace}', replace, content, flags = re.MULTILINE)
-                    if content != content_new:
-                        info(rf'>> Occurrences of {oldNamespace} replaced by {newNamespace} in {filePath}')
-                        f.seek(0)
-                        f.write(content_new)
-                        f.truncate()
+            content_new = re.sub(rf'(?i){oldNamespace}', replace, content, flags = re.MULTILINE)
+            if content != content_new:
+                info(rf'>> Occurrences of "{oldNamespace}" replaced by "{newNamespace}" in {filePath}')
+                f.seek(0)
+                f.write(content_new)
+                f.truncate()
 
 info('Executed successfully.')
 input('Press any key to exit...')
